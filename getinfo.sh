d1c76a74aa469587e4a2636fba500465b270fbec
Tuan Pham <71217312+tuanpham435@users.noreply.github.com>
2024-06-19 15:10:19 +0700

    Update getinfo.sh

--- getinfo.sh
+++ getinfo.sh
@@ -3,7 +3,7 @@
 cpu_num=$(grep -c ^processor /proc/cpuinfo)
 echo "cpu num: $cpu_num"
 
-total_memory=$(free -h | awk '/^Mem:/ {print $2}')
+total_memory=$(free -g | awk '/^Mem:/ {print $2}')
 free_memory=$(free -m | awk '/^Mem:/ {print $4}')
 echo "memory total: $total_memory"
 echo "memory free: $free_memory M"
